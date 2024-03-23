73c7dd822f077c08abbb31879e1be75e78a3fd6e
andreymudri <andreybeckert@gmail.com>
2024-03-23 19:55:24 -0300

    added base classes for the models

---
+++ models/discipline.py
@@ -0,0 +1,7 @@
+
+
+class Discipline:
+  def __init__(self, name, teacher):
+    self.name = name
+    #TODO checar se professor existe na lista/json
+    self.teacher = teacher
---
+++ models/enrollment.py
@@ -0,0 +1,5 @@
+class Enrollment():
+    def __init__(self, student, course):
+      #TODO checar se estudante e curso existem na lista/json
+        self.student_id = student
+        self.course_id = course
